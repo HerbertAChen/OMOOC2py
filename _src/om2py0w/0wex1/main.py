69aa062049c1b12462fd1de23b7b7f29f32eeecb
sunoonlee <helloliming@gmail.com>
2015-10-19 23:36:10 +0800

    filename as a global var; formatted string

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 # 0wex1 EasyNote
 from datetime import datetime
+filename = 'mynotes.txt'
 
 def main():
-	print '\n### EasyNote ###'
-	prompt = '''[A]: adding a new note
-[L]: listing all notes
-[H]: help
-[E]: exit
+	head = '\n### EasyNote ###' 
+	prompt = '''
+[A] adding a new note
+[L] listing all notes
+[H] help
+[E] exit
  '''
- 	helpmsg = '''#### Help ####
- 1. The notes are stored in log.txt in current directory.
- 2. For more info, please contact sunoonlee@github.
+ 	helpmsg = '''
+ #### Help ####
+ 1. The notes are stored in mynotes.txt in current directory.
+ 2. For more info, please refer to README.md.
  '''
+	print head
 	input = raw_input(prompt).upper()
 	while (input != 'E'):
 		if input == 'A':
@@ -27,20 +31,20 @@ def main():
 		input = raw_input(prompt).upper()
 
 def AddNote():
-	f = open('log.txt','a')
+	f = open(filename,'a')
 	newnote = raw_input('New note:\n> ')
-	curtimedate = str(datetime.now())[0:19]
-	f.write('[' + curtimedate + '] ' + newnote + '\n')
+	curdatetime = str(datetime.now())[0:19]
+	f.write('[%s] %s\n' % (curdatetime, newnote))
 	f.close()
 	print 'Note added!'
 
 def ListNotes():
-	f = open('log.txt','r')
+	f = open(filename,'r')
 	notes = f.readlines()
 	print '#### List of Notes ####\n'
 	for note in notes:
-		print '  '+note
+		print note
 	print '#### Total Num: %d ####' % len(notes)
 
 if __name__=='__main__':
- 	 main()      
+ 	 main()
