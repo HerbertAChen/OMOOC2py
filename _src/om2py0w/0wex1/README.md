c858ccc19ce6d5a9ecc39a138bd82f72c6a816aa
sunoonlee <helloliming@gmail.com>
2015-10-27 14:52:19 +0800

    easynote: 弃用shelve模块; 数据类型由字典改回列表; 增加标签识别

--- _src/om2py0w/0wex1/README.md
+++ _src/om2py0w/0wex1/README.md
@@ -1,80 +1,80 @@
-## 0wex1 EasyNote 说明书
+## 0wex1 EasyNote 说明书 v1.0
 
 ### 主要功能
 
-记录单行日记
-
-列出过往日记
+* 记录单行笔记
+    - 自动记录当前时间
+    - 支持标签输入
+* 列出过往笔记
+    - 列出全部笔记
+    - 按标签筛选笔记
+* 计划开发的功能
+    - 按日期范围筛选笔记
+    - 笔记删除
+    - 笔记按日/月/标签等分组显示
 
 ### 程序介绍
 
-输入的日记文本将存储在程序所在目录下的 mynotes.txt 文件中
-
-运行程序后，用户有如下4个选项：
-
-* A: add note
-* L: list notes
-* H: help
-* E: exit
-
-保存日记时，程序会自动在文本之前添加当前日期和时间，如：
-> [2015-10-18 22:41:07] This is a note.
-
-### 程序代码
-
+#### 交互界面与命令说明
+运行 `main.py` 后，进入程序交互界面:
 ```
-# -*- coding: utf-8 -*-
-# 0wex1 EasyNote
-from datetime import datetime
-filename = 'mynotes.txt'
-
-def main():
-	head = '\n### EasyNote ###' 
-	prompt = '''
-[A] adding a new note
-[L] listing all notes
-[H] help
-[E] exit
- '''
- 	helpmsg = '''
- #### Help ####
- 1. The notes are stored in mynotes.txt in current directory.
- 2. For more info, please refer to README.md.
- '''
-	print head
-	input = raw_input(prompt).upper()
-	while (input != 'E'):
-		if input == 'A':
-			AddNote()
-		elif input == 'L':
-			ListNotes()
-		elif input == 'H':
-			print helpmsg
-		else:
-			print 'Warning: Not a legal option!\n'
-		raw_input('Press Enter to continue')
-		input = raw_input(prompt).upper()
-
-def AddNote():
-	f = open(filename,'a')
-	newnote = raw_input('New note:\n> ')
-	curdatetime = str(datetime.now())[0:19]
-	f.write('[%s] %s\n' % (curdatetime, newnote))
-	f.close()
-	print 'Note added!'
-
-def ListNotes():
-	f = open(filename,'r')
-	notes = f.readlines()
-	print '#### List of Notes ####\n'
-	for note in notes:
-		print note
-	print '#### Total Num: %d ####' % len(notes)
-
-if __name__=='__main__':
- 	 main()   
+## EasyNote v1.0 ##
+Type "help" for more infomation.
+$
 ```
 
-### 进展
+用户可根据需要输入不同的命令. 关于命令的说明可输入 help 查看.
+```
+### 命令说明
+* "new" -- 创建新笔记
+* "exit" -- 退出
+* "list [笔记集合]" -- 列出指定的笔记集合
+* "del [笔记集合]" -- 删除指定的笔记集合
+* 注：
+    1. [笔记集合]可以是 "all" 或 "笔记编号(多个编号用逗号隔开)"
+       或 "date:[yymmdd]-[yymmdd]" 或 "tag:[标签名]"
+    2. [笔记集合] 中不应包含空格!
+```
+**!注: 目前仅支持 `new`, `exit`, `list`, 及 `list tag:xxx`.**
 
-151018 创建文档
+#### 新建笔记
+* 输入命令 `new` 或 `n` , 将进入新建笔记界面. 
+* 可在笔记末尾用'#'输入标签. 
+* 输入完毕后按回车结束并保存, 程序会提示"note added!"
+```
+$ n
+New note:
+>>> 继续刷笨办法! #todo
+Note added!
+```
+
+#### 列出笔记
+输入命令 `list` 或 `list all`, 程序将在屏幕上打印出所有笔记.
+```
+$ list
+
+ No            CreateTime   Content
+ --            ----------   -------
+  0   2015-10-27 14:40:01   继续刷笨办法! #todo
+  1   2015-10-27 14:40:30   熟悉字符串的常用方法 #todo
+  2   2015-10-27 14:42:05   为何用 sublime text 打开 mynotes.txt 会显示乱码? #q
+  3   2015-10-27 14:43:08   勤奋 专注 持久 创意 良善
+
+     4 notes listed.       
+```
+
+也可选择仅列出包含某个标签的笔记, 输入命令 `list tag:[tagname]` 即可:
+```
+$ list tag:todo                                             
+                                                            
+ No            CreateTime   Content                         
+ --            ----------   -------                         
+  0   2015-10-27 14:40:01   继续刷笨办法! #todo                   
+  1   2015-10-27 14:40:30   熟悉字符串的常用方法 #todo                
+                                                            
+     2 notes listed.                                        
+```
+
+#### 其他
+
+输入的笔记将自动存储在程序所在目录下的 mynotes.txt 文件中
--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -3,6 +3,8 @@
 from datetime import datetime
 import shelve
 
+filename = "mynotes.txt"
+
 def main():
 	head = '''
 ## EasyNote v1.0 ##
@@ -10,106 +12,98 @@ Type "help" for more infomation.'''
 	
 	helpmsg = u'''
 ### 命令说明
-* "new" -- 创建新笔记
+* "new" -- 新建笔记
 * "exit" -- 退出
 * "list [笔记集合]" -- 列出指定的笔记集合
 * "del [笔记集合]" -- 删除指定的笔记集合
-* "exp [笔记集合] [filename].txt" -- 把指定的笔记集合导出到指定文件
 * 注：
     1. [笔记集合]可以是 "all" 或 "笔记编号(多个编号用逗号隔开)"
        或 "date:[yymmdd]-[yymmdd]" 或 "tag:[标签名]"
-    2. [笔记集合] 或 [文件名] 中均不应包含空格!
+    2. [笔记集合] 中不应包含空格!
 '''	
-	
 	print head
 	cmd = raw_input('$ ').lower().strip()
-	database = shelve.open('mynotes.dat', flag='c', protocol=None, writeback=True)
-	try:
-		while (cmd not in ['exit','quit','q']):
-			cmdwords = cmd.split(' ') # 分解用户指令
-			length = len(cmdwords)
+	while (cmd not in ['exit','quit','q']):
+		cmdwords = cmd.split(' ') # 分解用户指令
+		length = len(cmdwords)
 
-			if cmd == 'help':
-				print helpmsg
-			elif cmd == 'new':
-				NewNote(database)
-			elif cmd == 'list':
-				ListNotes(database, 'all')
-			elif length == 2:
-				word1, word2 = cmdwords[0], cmdwords[1]
-				if word1 == 'list':
-					ListNotes(database, word2)
-				#elif word1 == 'del':
-				#	DelNotes(database, word2)
-				else:
-					print 'Invalid command!\n'
-			#elif length == 3:
-			#	word1, word2, word3 = cmdwords[0], cmdwords[1], cmdwords[2]
-			#	if word1 == 'exp':
-			#		ExpNotes(database, word2, word3)
-			#	else:
-			#		print 'Invalid command!\n'
+		if cmd in ['help', 'h', '?']:
+			print helpmsg
+		elif cmd in ['new', 'n']:
+			NewNote()
+		elif cmd == 'list':
+			ListNotes('all')
+		elif length >= 2:
+			word1, word2 = cmdwords[0], cmdwords[1]
+			if length == 2 and word1 == 'list':
+				ListNotes(word2)
+			#elif length == 2 and word1 == 'del':
+			#	DelNotes(word2)
 			else:
 				print 'Invalid command!\n'
-			cmd = raw_input('$ ').lower().strip()
-	finally:
-		database.close()
+		else:
+			print 'Invalid command!\n'
+		cmd = raw_input('$ ').lower().strip()
 		
-def NewNote(db):
-	key = str(len(db)+1)
-	note = {}
-	input = raw_input('New note:\n>>> ')
-	note['content'] = input
-	#note['content'] = input.partition('#')[0]
-	#note['tag'] = input.partition('#')[2]
-	note['datetime'] = str(datetime.now())[0:19]
-	db[key] = note
-	#print db #测试用
-	print 'Note added!'
+def NewNote():
+	f = open(filename,'a')
+	note  = raw_input('New note:\n>>> ').strip()
+	if input != '':
+		time = str(datetime.now())[0:19] # 去掉秒数的小数部分
+		f.write('%s   %s\n' % (time, note))
+		f.close()
+		print 'Note added!'
 
-def ListNotes(db, filter):
-	notes = SelectNotes(db, filter)
-	if notes != -1:
-		print '\n#### List of Notes ####\n'
-		for key in notes:
-			note = notes[key]
-			print '[%s] %s ' % (
-				note['datetime'], note['content'])
-		print '\n#### Total Num: %d ####' % len(notes)
+def ListNotes(filter):
+	result = SelectNotes(filter)
+	if result != -1:
+		selected, indexes = result[0], result[1]
+		if len(selected) == 0:
+			print 'Nothing selected!中文' # 不加u会显示乱码, 为什么!
+		else:
+			print '\n%s   %s   %s' % (
+				' No', ' '*9+'CreateTime', 'Content')
+			print '%s   %s   %s' % (' --', ' '*9+'-'*10, '-'*7) 
+			for i in range(len(selected)):
+				print '% 3d   %s' % (indexes[i], selected[i]),
+			print '\n     %d notes listed.\n' % len(selected)
 		
 #def DelNotes(db, filter):
-#	print 'notes deleted!' #测试用
-#
-#def ExpNotes(db, filter, filename):
-#	print 'notes exported!' #测试用
 
 #def Group():
 
-def SelectNotes(db, filter): #输入filter，返回 selected notes
+def SelectNotes(filter): #输入filter，返回符合条件的笔记编号和内容
 	# 如何区分两种意外情况：1、错误输入，2、未找到符合条件的note
-	selected = {}
+	f = open(filename,'r')
+	notes = f.readlines()
+	f.close()
+	selected, indexes = [], []
 	if filter == 'all':
-		return db
+		selected = notes
+		indexes = range(len(notes))
+		return [selected, indexes]
 #	elif filter.split(',') 都是 digit:
-#		if 满足条件：0 < num < len(db):
-#			for key in filter_nums：
-#				selected[key] = db[key]
-#			return selected
+#		if 0 < num < len(notes):
+#			for id in filter_nums：
+#				selected[id] = db[id]
+#			return [selected, indexes]
 #		else:
 #			print 'Note_index out of range.'
 #			return -1
-#	elif filter[0:3] = 'tag:':
-#		tag = filter[4:]
-#		if tag in tags:
-#			selected = notes with this tag
-#			return selected
-#		else:
-#			print 'tag doesn\'t exist!'
-#			return -1
+	elif filter[0:4] == 'tag:':
+		tag = filter[4:]
+		for i in range(len(notes)):
+			note = notes[i]
+			if '#' in note:
+				if note.partition('#')[2][0:-1].lower() == tag: 
+				# 务必注意, 在比较时去掉note末尾的\n
+					selected.append(note)
+					indexes.append(i)
+		return [selected, indexes]
 #	elif (filter[0:4] = 'date:'):
 #		if filter[5:] 为有效的日期格式:
 #			selected = notes within this date range
-#			return selected
+#			return [selected, indexes]
 #		else:
 #			print 'Invalid date format!'
 #			return -1
