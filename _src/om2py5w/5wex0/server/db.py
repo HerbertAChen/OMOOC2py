6e7e7418185a66a053d0002d69f696cb50caeb16
sunoonlee <helloliming@gmail.com>
2015-11-26 23:55:00 +0800

    sunote v1 大致就这样了

--- _src/om2py5w/5wex0/server/db.py
+++ _src/om2py5w/5wex0/server/db.py
@@ -4,7 +4,7 @@
 Database operation module.
 '''
 from datetime import datetime
-import sae.kvdb
+import sae.kvdb 
 
 def key_plus_one(last_key):
 	new_indx = int(last_key[4:]) + 1
@@ -13,7 +13,7 @@ def key_plus_one(last_key):
 
 def AddNote(text): # 把新笔记写入 kvdb
 	kv = sae.kvdb.Client()
-	keys = kv.getkeys_by_prefix('note')
+	keys = [key for key in kv.getkeys_by_prefix('note') if key[4:].isdigit()]
 	keys.sort()
 	new_key = key_plus_one(keys[-1]) if keys else 'note00001'
 	time = str(datetime.now())
@@ -21,9 +21,9 @@ def AddNote(text): # 把新笔记写入 kvdb
 	kv.set(new_key, new_note)
 	kv.disconnect_all()
 
-def GetNotes(): # 从 kvdb 获取全部笔记的列表
+def GetNotes(): #  从 kvdb 获取全部笔记的列表
 	kv = sae.kvdb.Client()
-	keys = kv.getkeys_by_prefix('note')
+	keys = [key for key in kv.getkeys_by_prefix('note') if key[4:].isdigit()]
 	keys.sort()
 	notes = [kv.get(key) for key in keys]
 	return notes if notes else [{}]
@@ -31,7 +31,8 @@ def GetNotes(): # 从 kvdb 获取全部笔记的列表
 
 def GetNum():
 	kv = sae.kvdb.Client()
-	notes_num = len(kv.getkeys_by_prefix('note'))
+	keys = [key for key in kv.getkeys_by_prefix('note') if key[4:].isdigit()]
+	notes_num = len(keys)
 	kv.disconnect_all()
 	return notes_num
 
@@ -39,7 +40,7 @@ def Delete(i):
 	num = GetNum()
 	if -num <= i < num:
 		kv = sae.kvdb.Client()
-		keys = kv.getkeys_by_prefix('note')
+		keys = [key for key in kv.getkeys_by_prefix('note') if key[4:].isdigit()]
 		keys.sort()
 		kv.delete(keys[i])
 		kv.disconnect_all()
@@ -48,7 +49,7 @@ def Delete(i):
 
 def DeleteAll():
 	kv = sae.kvdb.Client()
-	keys = kv.getkeys_by_prefix('note')
+	keys = [key for key in kv.getkeys_by_prefix('note') if key[4:].isdigit()]
 	for key in keys:
 		kv.delete(key)
 	kv.disconnect_all()
--- _src/om2py5w/5wex0/server/myapp.py
+++ _src/om2py5w/5wex0/server/myapp.py
@@ -5,7 +5,7 @@ from flask import Flask, request, redirect, render_template
 import db
 
 app = Flask(__name__)
-app.debug = True
+app.debug = True 
 
 @app.route('/', methods=['GET', 'POST'])
 def Note():
@@ -22,7 +22,6 @@ def Note():
 				db.Delete(-j)
 			else:
 				db.AddNote(text)
-
 		return redirect('/')
 	notes = db.GetNotes()
 	notes_reverse = list(reversed(notes))
@@ -30,13 +29,13 @@ def Note():
 
 @app.route('/tags') # 待完善：标签浏览和管理
 def Export():
-	info = u' \n功能开发中...'
+	info = u' \n 功能开发中...'
 	return render_template('info.html', info=info)
 
 @app.route('/about')
 def ShowAbout():
 	info = u' \n极简笔记本应用 by sunoonlee\n\
-		- for omooc2py 5wex0 \nContact:\n- Github/weibo: sunoonlee\n\
+		- for omooc2py 5wex0  \nContact:\n- Github/weibo: sunoonlee\n\
 		- Email: helloliming@gmail.com'
 	return render_template('info.html', info=info)
 
