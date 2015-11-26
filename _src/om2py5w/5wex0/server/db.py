77481422702eb13dd0f5a0f374092feaa87d621b
sunoonlee <helloliming@gmail.com>
2015-11-26 19:49:01 +0800

    one key one note; add delete function

--- _src/om2py5w/5wex0/server/db.py
+++ _src/om2py5w/5wex0/server/db.py
@@ -6,35 +6,49 @@ Database operation module.
 from datetime import datetime
 import sae.kvdb
 
-def NewNote(text): # 把新笔记写入 kvdb
+def key_plus_one(last_key):
+	new_indx = int(last_key[4:]) + 1
+	new_key = last_key[:4] + '%05u' % new_indx
+	return new_key
+
+def AddNote(text): # 把新笔记写入 kvdb
 	kv = sae.kvdb.Client()
-	notes = kv.get('notes')
-	if not notes: notes = []
-
+	keys = kv.getkeys_by_prefix('note')
+	keys.sort()
+	new_key = key_plus_one(keys[-1]) if keys else 'note00001'
 	time = str(datetime.now())
-	new_note = {'content':text, 'time':time}
-	notes.append(new_note)
-
-	kv.set('notes', notes)
-	#if text == 'cleaR':
-	#	kv.delete('notes')
+	new_note = {'text':text, 'time':time}
+	kv.set(new_key, new_note)
 	kv.disconnect_all()
 
 def GetNotes(): # 从 kvdb 获取全部笔记的列表
 	kv = sae.kvdb.Client()
-	allnotes = kv.get('notes')
-	result = allnotes if allnotes else [{}]
-	return result
+	keys = kv.getkeys_by_prefix('note')
+	keys.sort()
+	notes = [kv.get(key) for key in keys]
+	return notes if notes else [{}]
 	kv.disconnect_all()	
 
 def GetNum():
 	kv = sae.kvdb.Client()
-	notes = kv.get('notes')
-	notes_num = len(notes) if notes else 0
+	notes_num = len(kv.getkeys_by_prefix('note'))
 	kv.disconnect_all()
 	return notes_num
 
-def delete_all():
+def Delete(i):
+	num = GetNum()
+	if -num <= i < num:
+		kv = sae.kvdb.Client()
+		keys = kv.getkeys_by_prefix('note')
+		keys.sort()
+		kv.delete(keys[i])
+		kv.disconnect_all()
+	else:
+		pass
+
+def DeleteAll():
 	kv = sae.kvdb.Client()
-	kv.delete('notes')
+	keys = kv.getkeys_by_prefix('note')
+	for key in keys:
+		kv.delete(key)
 	kv.disconnect_all()
--- _src/om2py5w/5wex0/server/myapp.py
+++ _src/om2py5w/5wex0/server/myapp.py
@@ -10,8 +10,19 @@ app.debug = True
 @app.route('/', methods=['GET', 'POST'])
 def Note():
 	if request.method == 'POST':
-		text = request.form['newnote']
-		if text: db.NewNote(text)
+		if request.form['newnote']:
+			text = request.form['newnote']
+			if text == '$d':
+				db.Delete(-1)
+			elif text.startswith('$d ') and text[3:].isdigit():
+				i = int(text[3:])
+				db.Delete(i)
+			elif text.startswith('$d -') and text[4:].isdigit():
+				j = int(text[4:])
+				db.Delete(-j)
+			else:
+				db.AddNote(text)
+
 		return redirect('/')
 	notes = db.GetNotes()
 	notes_reverse = list(reversed(notes))
@@ -30,7 +41,7 @@ def ShowAbout():
 	return render_template('info.html', info=info)
 
 @app.route('/count') # 统计笔记数量
-def Admin():
+def Count():
 	notes_num = db.GetNum()
 	info = ' \n%s notes in total.' % notes_num
 	return render_template('info.html', info=info)
